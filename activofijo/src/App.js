0cfa806d6a30e2cac49b21b9e9f01759ff9942aa
AlessandroHACK <alessandro.verdin215@gmail.com>
2023-09-28 15:44:59 -0600

    Pdf v2

--- activofijo/src/App.js
+++ activofijo/src/App.js
@@ -1,9 +1,10 @@
-// App.js
 import React, { useState } from 'react';
 import './App.css';
 import ProductLabel from './components/ProductLabel';
 import ProductForm from './components/ProductForm';
-
+import { saveAs } from 'file-saver';
+import { Document, Page, View, Text, pdf } from '@react-pdf/renderer'; 
+import Barcode from 'react-barcode';
 function App() {
   const [products, setProducts] = useState([]);
 
@@ -12,7 +13,6 @@ function App() {
   };
 
   const handleClearProduct = (indexToRemove) => {
-    // Crea una copia del array de productos sin el elemento en el índice especificado.
     const updatedProducts = products.filter((_, index) => index !== indexToRemove);
     setProducts(updatedProducts);
   };
@@ -21,12 +21,31 @@ function App() {
     setProducts([]);
   };
 
+  const downloadAllBarcodesPDF = () => {
+    const blobStream = pdf((
+      <Document>
+        {products.map((product, index) => (
+          <Page key={index} size="A4">
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+              <Barcode value={product.code} height={40} />
+              <Text>{product.code}</Text>
+            </View>
+          </Page>
+        ))}
+      </Document>
+    )).toBlob();
+
+    blobStream.then((blob) => {
+      saveAs(blob, 'All_Barcodes.pdf');
+    });
+  };
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6">
           <div className="card">
-            <div className="card-body text-center"> 
+            <div className="card-body text-center">
               <h2 className="card-title">Generador de Activos Fijos</h2>
               <ProductForm onSave={handleSaveProduct} />
             </div>
@@ -34,11 +53,15 @@ function App() {
         </div>
         <div className="col-md-6">
           <div className="card">
-            <div className="card-body text-center"> 
+            <div className="card-body text-center">
               <h2 className="card-title">Activos Fijos Generados</h2>
               <button onClick={handleClearProducts} className="btn btn-danger mb-3">
-              <i className="fa fa-trash"></i> 
+                <i className="fa fa-trash"></i>
                 Clear All
+              </button>
+              <button onClick={downloadAllBarcodesPDF} className="btn btn-success mb-3">
+                <i className="fa fa-file-pdf-o"></i>
+                Download All Barcodes PDF
               </button>
               <div className="list-group">
                 {products.map((product, index) => (
